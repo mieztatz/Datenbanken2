c5ffade8e55024ae2f5cc342447b11513cbeb7b7
Fabian Uhlmann <fuhlmann@hm.edu>
2016-04-21 13:47:15 +0200

    Insert new Newsletter User is possible now

--- newsletter/newsletter.php
+++ newsletter/newsletter.php
@@ -4,32 +4,26 @@
 		<meta http-equiv="content-type" content="text/html; charset=utf-8">
 		<title> NEWSLETTER </title>
  		<meta name="viewport" content="width=device-width, initial-scale=1">
-  		<link rel="stylesheet" href="styles.css">
-   		
-  
-		
+  		<link rel="stylesheet" href="../styles.css">
 	</head>
 	<body>
 		<div id='cssmenu'>
 			<ul>
-  				<li class='active'><a href='../index.php'>Voting</a></li>
+  				<li><a href='../index.php'>Voting</a></li>
    				<li><a href='../result.php'>Results</a></li>
-				<li><a href='newsletter.php'>Newsletter</a></li>
+				<li class='active'><a href='newsletter.php'>Newsletter</a></li>
 			</ul>
 		</div>
 		<h1> Newsletter/Mailinglisten abnonieren </h1>
-		<p> Hier kannst du für einen Fußballverein den Newsletter abonnieren. </p>
-		
+		<p> Hier kannst du für eine oder mehere Fußballigen den Newsletter abonnieren. </p>
+
 		<form name="data" method="POST" action="userdata.php">
 			<table>
 				<tr>
-					<td> NACHNAME* </td> <td> <input type='text' name='lastname'/> </td>
+					<td> VORNAME* </td> <td> <input type='text' name='firstname'/> </td>
 				</tr>
 				<tr>
-					<td> VORNAME*  </td> <td> <input type='text' name='firstname' /> </td>
-				</tr>
-				<tr>
-					<td> GEBURTSTAG</td> <td> <input type='text' name='birth' /> </td>
+					<td> NACHNAME*  </td> <td> <input type='text' name='lastname' /> </td>
 				</tr>
 				<tr>
 					<td> E-MAIL*   </td> <td> <input type='text' name='mail' /> </td>
@@ -37,15 +31,15 @@
 				<tr>
 					<td> VEREINSMITGLIED*   </td>
 					<td> <select name="member">
-							<option value="Ja"> JA
+							<option value="JA"> JA
 							<option vale="NEIN"> NEIN
 						</select>
 					</td>
 				</tr>
-			</table>
-			<p> <input type='submit' value='Abonnieren'/> <p>	
+      </table>
+			<p> <input type='submit' value='Daten eintragen'/> <p>
 		</form>
-		
-		
+
+
 	</body>
 </html>
--- newsletter/userdata.php
+++ newsletter/userdata.php
@@ -3,19 +3,65 @@
     <head>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8">
 		<title> NEWSLETTER </title>
+ 		<meta name="viewport" content="width=device-width, initial-scale=1">
+  		<link rel="stylesheet" href="../styles.css">
 	</head>
+  <body>
+		<div id='cssmenu'>
+			<ul>
+  				<li><a href='../index.php'>Voting</a></li>
+   				<li><a href='../result.php'>Results</a></li>
+				<li class='active'><a href='newsletter.php'>Newsletter</a></li>
+			</ul>
+		</div>
+
+    <p>
+    <h1>  Newsletter </h1>
+    Hier können Sie die gewünschten Newsletter der einzelnen Fußballkategorien auswählen.
+  </p>
+
 <?php
+  include ("../connection.php");
 
-	$lastname = $_POST['lastname'] == "" ? $_POST['lastname'] : noinput();
-	$firstname = $_POST['firstname'] == "" ? $_POST['firstname'] : noinput();
-	$firstname = $_POST['firstname'] == "" ? $_POST['firstname'] : noinput();
-	$birth = $_POST['birth'];
-	$mail = $_POST['mail'] == "" ? $_POST['mail'] : noinput();
-	$member = $_POST['member'] == "" ? $_POST['member'] : noinput();
+  $sqlIns = "INSERT INTO fans (firstname,lastname,mail,clubmember) VALUES (?,?,?,?)";
+  $stmt = $mysqli->prepare($sqlIns);
+  $stmt->bind_param("sssi",$lastname,$firstname,$mail,$member);
 
-	function noinput() {
-		echo "Bitte alle mit * gekennzeichneten Felder ausfüllen.";
+	$lastname = !empty($_POST['lastname']) ? $_POST['lastname'] : noinput();
+	$firstname = !empty($_POST['firstname']) ? $_POST['firstname'] : noinput();
+  $mail = !empty($_POST['mail']) ? $_POST['mail'] : noinput();
+	$member = $_POST['member'] == "JA" ? (int)1 : (int)0;
+
+
+  if(!$stmt->execute()){
+    echo "Error";
+    $stmt->close();
+  }
+  else{
+
+    $sqlReq = "SELECT * FROM league";
+    $stmt = $mysqli->prepare($sqlReq);
+    if($stmt){
+      $stmt->execute();
+      $stmt->bind_result($result);
+
+      echo "<table>";
+         while ($stmt->fetch()) {
+             echo "<tr><td><input type='checkbox' value='$result'> $result</td></tr>";
+         }
+      echo "</table>";
+    }
+    $stmt->close();
+  }
+  $mysqli->close();
+
+  function noinput() {
+		echo "<p>Bitte alle mit * gekennzeichneten Felder ausfüllen.</p>";
+    $mysqli->close();
+    exit();
 	}
 
-?>
+  ?>
+
+</body>
 </html>
