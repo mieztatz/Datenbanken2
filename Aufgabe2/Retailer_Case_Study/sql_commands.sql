b55cc86d37811bc09400a763ab6c5d73873d4cd7
Fabian Uhlmann <fuhlmann@hm.edu>
2016-05-29 15:03:50 +0200

    Fortsetzung Objektorientiertes Relationship

--- Aufgabe2/Retailer_Case_Study/sql_commands.sql
+++ Aufgabe2/Retailer_Case_Study/sql_commands.sql
@@ -46,7 +46,7 @@ Market  VARCHAR2(10)) NOT FINAL;
 -----------------------------------------------------
 --  DDL for Type SHAREHOLDER_T
 -----------------------------------------------------
-CREATE OR REPLACE TYPE "Shareholder_T" AS OBJECT(
+CREATE OR REPLACE TYPE "SHAREHOLDER_T" AS OBJECT(
 Sholder_ID  INTEGER,
 Sholder_Name VARCHAR2(30),
 Sholder_Contact REF Contact_T)) NOT FINAL;
@@ -95,7 +95,7 @@ Yearly_Salary VARCHAR2(10) NOT FINAL;
 --------------------------------------------------------
 --  DDL for Type MANAGEMENT_T
 --------------------------------------------------------
-  CREATE OR REPLACE TYPE "OWN_SHARES_T" AS OBJECT(
+  CREATE OR REPLACE TYPE "MANAGEMENT_T" AS OBJECT(
 Manag_ID INTEGER,
 Manag_Name VARCHAR2(30),
 Manag_Contact REF Contact_T,
@@ -110,6 +110,97 @@ Manag_Typ_Manager MANAGERS) NOT FINAL;
 
 /
 --------------------------------------------------------
+--  DDL for Type FULLTIME_T
+--------------------------------------------------------
+  CREATE OR REPLACE TYPE "FULLTIME_T" AS OBJECT(
+Annual_Wage VARCHAR2(7),
+Emp_Bonus VARCHAR2(6)) NOT FINAL;
+
+/
+--------------------------------------------------------
+--  DDL for Type FULLTIME
+--------------------------------------------------------
+  CREATE OR REPLACE TYPE "FULLTIME" as table of Fulltime_t
+
+/
+--------------------------------------------------------
+--  DDL for Type PARTTIME_T
+--------------------------------------------------------
+  CREATE OR REPLACE TYPE "PARTTIME_T" AS OBJECT(
+Weekly_Wage VARCHAR2(7)) NOT FINAL; -- ggf. als CREATE OR REPLACE TYPE "PARTTIME_T" as table of VARCHAR2(7), weil nur 1 Attribut in der Tabelle vorhanden
+
+/
+--------------------------------------------------------
+--  DDL for Type PARTTIME
+--------------------------------------------------------
+  CREATE OR REPLACE TYPE "PARTTIME" as table of Parttime_t
+
+/
+--------------------------------------------------------
+--  DDL for Type CASUAL_T
+--------------------------------------------------------
+  CREATE OR REPLACE TYPE "CASUAL_T" AS OBJECT(
+Hourly_Wage VARCHAR2(7)) NOT FINAL;
+
+/
+--------------------------------------------------------
+--  DDL for Type CASUAL
+--------------------------------------------------------
+  CREATE OR REPLACE TYPE "CASUAL" as table of Casual_t
+
+/
+--------------------------------------------------------
+--  DDL for Type EMPLOYEE_T
+--------------------------------------------------------
+  CREATE OR REPLACE TYPE "EMPLOYEE_T" AS OBJECT(
+Emp_ID VARCHAR2(5),
+Emp_Name VARCHAR2(30),
+Emp_Contact REF Contact_T,
+Emp_Full_Time FULLTIME,
+Emp_Part_Time PARTTIME,
+Emp_Casual CASUAL) NOT FINAL;
+
+/
+--------------------------------------------------------
+--  DDL for Type EMPLOYEES
+--------------------------------------------------------
+  CREATE OR REPLACE TYPE "EMPLOYEES" as table of Employee_t
+
+/
+--------------------------------------------------------
+--  DDL for Type DEPARTMENT_T
+--------------------------------------------------------
+  CREATE OR REPLACE TYPE "DEPARTMENT_T" AS OBJECT(
+Dept_ID INTEGER,
+Dept_Name VARCHAR2(20) INTEGER,
+Dept_Head VARCHAR2(25)
+Dept_Employees EMPLOYEES) NOT FINAL;
+
+/
+--------------------------------------------------------
+--  DDL for Type DEPARTMENTS
+--------------------------------------------------------
+  CREATE OR REPLACE TYPE "DEPARTMENTS" as table of Department_t
+
+/
+--------------------------------------------------------
+--  DDL for Type STORE_T
+--------------------------------------------------------
+  CREATE OR REPLACE TYPE "STORE_T" AS OBJECT(
+Store_ID VARCHAR2(5),
+Store_Location VARCHAR2(20) INTEGER,
+Store_Contact REF Contact_T,
+Store_Manage VARCHAR2(30),
+Store_Depts DEPARTMENTS) NOT FINAL;
+
+/
+--------------------------------------------------------
+--  DDL for Type STORES
+--------------------------------------------------------
+  CREATE OR REPLACE TYPE "STORES" as table of Store_t
+
+/
+--------------------------------------------------------
 --  DDL for Type COMPANY_T
 --------------------------------------------------------
 CREATE OR REPLACE TYPE "COMPANY_T" AS OBJECT(
@@ -118,8 +209,9 @@ Comp_Name VARCHAR2(20),
 Comp_Contact REF Contact_T,
 Comp_Typ1 COMPANYTYPE1, -- wie kann sichergestellt werden, dass die Firma nur einen der beiden Typen als NESTED TABLE beinhaltet? 
 Comp_Typ2 COMPANYTYPE2,
-Comp_Sholders SHAREHOlDERS,
-Comp_Manag MANAGEMENT) NOT FINAL;
+Comp_Sholders SHAREHOLDERS,
+Comp_Manag MANAGEMENT,
+Comp_Stores STORES) NOT FINAL;
 
 
 
