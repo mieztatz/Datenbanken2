aa9a3dd7a854f019ad9ec7ef57d75821c6749b82
ifw13044 <ifw13044@pn.cs.hm.edu>
2016-06-20 13:22:48 +0200

    Funktionen für Namensplitting

--- Aufgabe3/sql_befehle_aufg3.sql
+++ Aufgabe3/sql_befehle_aufg3.sql
@@ -40,7 +40,7 @@
 --  DDL for Table PERSONAL
 --------------------------------------------------------
   CREATE TABLE "PERSONAL"(
-	"P_NR" NUMBER, 
+	"P_NR" NUMBER NOT NULL AUTO_INCREMENT, 
 	"P_NAME" VARCHAR2(30), 
 	"P_VORNAME" VARCHAR2(30), 
 	"P_ALTER" NUMBER, 
@@ -59,4 +59,50 @@
 	"Z_KEY_OLD" VARCHAR2(60), 
 	PRIMARY KEY ("Z_NR"),
 	FOREIGN KEY ("Z_NR") REFERENCES "PERSONAL" ("P_NR"));
+	
+
+	
+--------------------------------------------------------
+--  DDL for Function GETLASTNAME
+--------------------------------------------------------
+CREATE OR REPLACE FUNCTION "GETLASTNAME" 
+  (lname VARCHAR2)
+RETURN VARCHAR2
+IS
+BEGIN
+  RETURN SUBSTR(lname,instr(lname,' ',0)+1);
+END;
+
+/
+--------------------------------------------------------
+--  DDL for Function GETFIRSTNAME
+--------------------------------------------------------
+CREATE OR REPLACE FUNCTION "GETFIRSTNAME" 
+  (fname VARCHAR2)
+RETURN VARCHAR2
+IS
+BEGIN
+  RETURN SUBSTR(fname,0, instr(fname,' ',0));
+END;
+
+/
+--------------------------------------------------------
+--  DDL for Procedure 
+--------------------------------------------------------
+  CREATE OR REPLACE PROCEDURE "transform" 
+IS
+p_name VARCHAR2;
+p_vorname VARCHAR2;
+CURSOR CANGST IS
+	SELECT A_Name, A_Geburtsdatum, A_Berufsbezeichnung, A_Monatsgehalt, A_Geschlecht
+	FROM Angestellte
+BEGIN
+  OPEN CANGST;
+  LOOP 
+	FETCH CANGST INTO p_name
+	EXIT WHEN CANGST%NOTFOUND;
+	INSERT INTO PERSONAL(p_name,p_vorname) VALUES (GETLASTNAME(p_name),GETFIRSTNAME(name);
+  END LOOP; 
+  CLOSE ANGST;
+END;
  
